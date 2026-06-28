95e1dbb30902f2d3ab4c30bfe91a6024e63a5ea1
Basanta <basanta.simkhada@alphabox.com.np>
2026-06-28 18:20:14 +0545

    cicd 2

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const PORT = 3000;
 
 const server = http.createServer((req, res) => {
   res.writeHead(200, { "Content-Type": "text/plain" });
-  res.end("Hello, World!\n\nTesting");
+  res.end("Hello, World!\n");
 });
 
 server.listen(PORT, HOST, () => {
