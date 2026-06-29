13ed1b848acd647ebe4cefb6532110c862f47b1c
rudra <rudra1@gmail.com>
2026-06-29 22:51:59 +0545

    cicd 2

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const PORT = 3000;
 
 const server = http.createServer((req, res) => {
   res.writeHead(200, { "Content-Type": "text/plain" });
-  res.end("Hello, World!\n");
+  res.end("Hello, World this is node app !\n");
 });
 
 server.listen(PORT, HOST, () => {
